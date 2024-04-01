a93098cc57b7eda4f8977a1c8dc57c1c8c729d3e
std-026-35 std-026-35 <std-026-35@praktikum-services.ru>
2024-04-01 07:55:23 +0000

    Flyway

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+
 #Если свалится одна из команд, рухнет и весь скрипт.
 set -xe
 #Создаем директорию для логов
@@ -9,8 +10,19 @@ sudo chown backend:backend /var/log/sausage-store/
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
 sudo rm -f /home/student/sausage-store.jar||true
 #Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+curl -u "${NEXUS_REPO_USER}":"${NEXUS_REPO_PASS}" -o sausage-store.jar "${NEXUS_REPO_URL}"/"${NEXUS_REPO_BACKEND_NAME}"/com/yandex/practicum/devops/sausage-store/"${VERSION}"/sausage-store-"${VERSION}".jar
 sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store-backend.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
+#Создаем файл .env с переменными
+cd /home/student
+rm .env
+cat > .env << END
+PSQL_DBNAME="${PSQL_DBNAME}"
+PSQL_HOST="${PSQL_HOST}"
+PSQL_PASSWORD="${PSQL_PASSWORD}"
+PSQL_PORT="${PSQL_PORT}"
+PSQL_USER="${PSQL_USER}"
+END
+sudo chown backend:backend .env
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
